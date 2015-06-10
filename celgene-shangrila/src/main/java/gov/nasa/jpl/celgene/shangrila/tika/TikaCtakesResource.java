7936b7f30c4245a4c0407617b62e9200857a3871
Chris Mattmann <mattmann@apache.org>
2015-06-10 13:26:43 -0700

    - initial commit of proxy for Tika service; next is cTAKES

---
+++ celgene-shangrila/src/main/java/gov/nasa/jpl/celgene/shangrila/tika/TikaCtakesResource.java
@@ -0,0 +1,30 @@
+package gov.nasa.jpl.celgene.shangrila.tika;
+
+import java.io.InputStream;
+
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+import org.apache.cxf.jaxrs.client.WebClient;
+
+@Path("/tika")
+public class TikaCtakesResource {
+  
+  public static final String PROXY_URL_TIKA = "http://localhost:9020";
+  
+  public static final String PROXY_URL_CTAKES = "http://localhost:9021";
+  
+  @PUT
+  @Path("/rmeta")
+  public Response forwardTika(InputStream is, @HeaderParam("Content-Dispotion") String contentDispotion){
+      Response response = 
+          WebClient.create(PROXY_URL_TIKA)
+          .accept("application/json")
+          .header("Content-Disposition", contentDispotion)
+          .put(is);
+      return response;    
+  }
+
+}
