78266b7cf5cb2c1ec68e3e7a7978317d5c79a2e7
Chris Mattmann <mattmann@apache.org>
2015-06-10 14:37:49 -0700

    - logging and fix URL to include /rmeta

--- celgene-shangrila/src/main/java/gov/nasa/jpl/celgene/shangrila/tika/TikaCtakesResource.java
+++ celgene-shangrila/src/main/java/gov/nasa/jpl/celgene/shangrila/tika/TikaCtakesResource.java
@@ -1,6 +1,7 @@
 package gov.nasa.jpl.celgene.shangrila.tika;
 
 import java.io.InputStream;
+import java.util.logging.Logger;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
@@ -15,16 +16,19 @@ import org.apache.cxf.jaxrs.client.WebClient;
 @Path("/tika")
 public class TikaCtakesResource {
 
-  public static final String PROXY_URL_TIKA = "http://localhost:9201";
+  public static final Logger LOG = Logger.getLogger(TikaCtakesResource.class
+      .getName());
 
-  public static final String PROXY_URL_CTAKES = "http://localhost:9202";
+  public static final String PROXY_URL_TIKA = "http://localhost:9201/rmeta";
+
+  public static final String PROXY_URL_CTAKES = "http://localhost:9202/rmeta";
 
   @GET
   @Path("/status")
   @Produces("text/html")
   public Response status() {
-    return Response.ok("<h1>This is Tika cTAKES Resource: running correctly</h1>")
-        .build();
+    return Response.ok(
+        "<h1>This is Tika cTAKES Resource: running correctly</h1>").build();
   }
 
   @PUT
@@ -32,10 +36,13 @@ public class TikaCtakesResource {
   @Produces("application/json")
   public Response forwardTika(InputStream is,
       @HeaderParam("Content-Dispotion") String contentDispotion) {
+    LOG.info("PUTTING document [" + contentDispotion + "] to Tika at :["
+        + PROXY_URL_TIKA + "]");
     Response response = WebClient.create(PROXY_URL_TIKA)
         .accept("application/json")
         .header("Content-Disposition", contentDispotion).put(is);
     String json = response.readEntity(String.class);
+    LOG.info("Response received: " + json);
     return Response.ok(json, MediaType.APPLICATION_JSON).build();
   }
 
