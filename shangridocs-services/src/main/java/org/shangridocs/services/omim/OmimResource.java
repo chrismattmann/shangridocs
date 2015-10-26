17661db4b290d458efb3bb1732a5dbb79513c6e5
Asitang Mishra <asitang@gmail.com>
2015-10-26 12:01:55 -0700

    Added support for Omim and Uniprot services

--- shangridocs-services/src/main/java/org/shangridocs/services/omim/OmimResource.java
+++ shangridocs-services/src/main/java/org/shangridocs/services/omim/OmimResource.java
@@ -43,198 +43,182 @@ import org.json.JSONObject;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
-
-
-
 @Path("/omim")
 public class OmimResource {
 
-	public static String omimkey;
+  public static String omimkey;
 
-	public OmimResource(@Context ServletContext sc) {
-		omimkey=sc.getInitParameter("org.shangridocs.omim.apikey");
-	}
+  public OmimResource(@Context ServletContext sc) {
+    omimkey = sc.getInitParameter("org.shangridocs.omim.apikey");
+  }
 
-	@GET
-	@Path("/search")
-	@Produces("application/json")
-	public Response createColumnArray(@QueryParam("query") String query) throws JSONException, IOException  {
+  @GET
+  @Path("/search")
+  @Produces("application/json")
+  public Response createColumnArray(@QueryParam("query") String query)
+      throws JSONException, IOException {
 
+    return Response.ok(omim(query), MediaType.APPLICATION_JSON).build();
 
+  }
 
+  private static String readAll(Reader rd) throws IOException {
+    StringBuilder sb = new StringBuilder();
+    int cp;
+    while ((cp = rd.read()) != -1) {
+      sb.append((char) cp);
+    }
+    return sb.toString();
+  }
 
-		return Response.ok(omim(query), MediaType.APPLICATION_JSON).build();
+  public static JSONObject readJsonFromUrl(String url) throws IOException,
+      JSONException {
+    InputStream is = new URL(url).openStream();
+    try {
+      BufferedReader rd = new BufferedReader(new InputStreamReader(is,
+          Charset.forName("UTF-8")));
+      String jsonText = readAll(rd);
+      JSONObject json = new JSONObject(jsonText);
+      return json;
+    } finally {
+      is.close();
+    }
+  }
 
-	}
+  public static String createStandardJson(String Search_link,
+      String Num_results, ArrayList<HashMap<String, String>> entries) {
 
-	
+    String json = "";
+    json += "{";
 
-	private static String readAll(Reader rd) throws IOException {
-	    StringBuilder sb = new StringBuilder();
-	    int cp;
-	    while ((cp = rd.read()) != -1) {
-	      sb.append((char) cp);
-	    }
-	    return sb.toString();
-	  }
+    json += "\"Num_results\":\"" + Num_results + "\",";
+    json += "\"Search_link\":\"" + Search_link + "\",";
+    json += "\"Response\":[";
+    for (HashMap<String, String> map : entries) {
+      json += "{";
+      json += "\"Title\":\"" + map.get("Title") + "\",";
+      map.remove("Title");
+      json += "\"ID\":\"" + map.get("ID") + "\",";
+      map.remove("ID");
+      json += "\"Description\":\"" + map.get("Description") + "\",";
+      // System.out.println("DEBUG"+map.get("Description"));
+      map.remove("Description");
+      json += "\"Detail_link\":\"" + map.get("Detail_link") + "\",";
+      map.remove("Detail_link");
+      json += "\"Properties\":{";
 
-	public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-	    InputStream is = new URL(url).openStream();
-	    try {
-	      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-	      String jsonText = readAll(rd);
-	      JSONObject json = new JSONObject(jsonText);
-	      return json;
-	    } finally {
-	      is.close();
-	    }
-	  }
-	
-	
-	public static String createStandardJson( String Search_link, String Num_results,ArrayList< HashMap<String,String>> entries){
-		
-		String json="";
-		json+="{";
-		
-		json+="\"Num_results\":\""+Num_results+"\",";
-		json+="\"Search_link\":\""+Search_link+"\",";
-		json+="\"Response\":[";
-		for(HashMap<String, String> map:entries){
-			json+="{";
-			json+="\"Title\":\""+map.get("Title")+"\",";
-			map.remove("Title");
-			json+="\"ID\":\""+map.get("ID")+"\",";
-			map.remove("ID");
-			json+="\"Description\":\""+map.get("Description")+"\",";
-			//System.out.println("DEBUG"+map.get("Description"));
-			map.remove("Description");
-			json+="\"Detail_link\":\""+map.get("Detail_link")+"\",";
-			map.remove("Detail_link");	
-			json+="\"Properties\":{";
-			
-			for(String key:map.keySet()){
-				json+="\""+key+"\":\""+map.get(key)+"\",";
-			}
-			//remove the last comma
-			if(String.valueOf(json.charAt(json.length()-1)).equals(","))
-			json = json.substring(0, json.length()-1);
-			
-			json+="}";
-			
-			json+="},";
-		}
-		//remove the last comma
-		if(String.valueOf(json.charAt(json.length()-1)).equals(","))
-		json = json.substring(0, json.length()-1);
-				
-		json+="]";
-		
-		json+="}";
-		
-		
-		
-		
-		return json.replace("\n", " ");
-	}
-	public static Document readXMLFromUrl(String url) throws IOException, JSONException, SAXException, ParserConfigurationException {
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        factory.setNamespaceAware(true);
-        return factory.newDocumentBuilder().parse(new URL(url).openStream());	   
-	  }
+      for (String key : map.keySet()) {
+        json += "\"" + key + "\":\"" + map.get(key) + "\",";
+      }
+      // remove the last comma
+      if (String.valueOf(json.charAt(json.length() - 1)).equals(","))
+        json = json.substring(0, json.length() - 1);
 
-	public static JSONArray readJsonArrayFromUrl(String url) throws IOException, JSONException {
-	    InputStream is = new URL(url).openStream();
-	    try {
-	      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-	      String jsonText = readAll(rd);
-	      JSONArray json = new JSONArray(jsonText);
-	      return json;
-	    } finally {
-	      is.close();
-	    }
-	  }
-	
-	public static String omim(String query) throws JSONException, IOException{
-		
-		
-		
-		String Search_link="";
-		String Num_results="";
-		ArrayList< HashMap<String,String>> entries=new ArrayList< HashMap<String,String>>();
-		
-		
-		
-		JSONObject json=readJsonFromUrl("http://api.omim.org/api/entry/search?search="+query+"&start=0&limit=5&apiKey="+omimkey+"&format=json");
+      json += "}";
 
-		JSONObject temp1=(JSONObject)json.get("omim");
-		JSONObject temp2=(JSONObject)temp1.get("searchResponse");
-		Integer totalresults=(Integer)temp2.get("totalResults");
-		JSONArray ar1=(JSONArray)temp2.get("entryList");
-		
-		for(int i=0;i<ar1.length();i++){
-		
-			HashMap<String,String> map=new HashMap<String, String>();
-			JSONObject temp4=(JSONObject)ar1.get(i);
-			JSONObject temp5=(JSONObject)temp4.get("entry");
-			JSONObject temp6=(JSONObject)temp5.get("titles");
-			String t2=(String)temp6.get("preferredTitle");
-			String t1=(String)temp5.get("mimNumber");
-			String t3="";
-			
-			JSONObject jsontemp=readJsonFromUrl("http://api.omim.org/api/entry?mimNumber="+t1+"&include=text:description&apiKey="+omimkey+"&format=json");
-			
-			JSONObject temp7=(JSONObject)jsontemp.get("omim");
-			JSONArray ar2=(JSONArray)temp7.get("entryList");
-			JSONObject temp9=(JSONObject)ar2.get(0);
-			JSONObject temp11=(JSONObject)temp9.get("entry");
-			try{
-			JSONArray ar3=(JSONArray)temp11.get("textSectionList");
-			JSONObject temp8=(JSONObject)ar3.get(0);
-			JSONObject temp10=(JSONObject)temp8.get("textSection");
-			t3=(String)temp10.get("textSectionContent");
-			
-			
-			
-			}
-			catch(Exception e){
-				
-			}
-			
-			//System.out.println("DEBUG"+"Title:"+t2);
-			//System.out.println("DEBUG"+"ID:"+t1);
-			//System.out.println("DEBUG"+"Description:"+t3);
-			//System.out.println("DEBUG"+"Detail_link:http://omim.org/entry/"+t1);
-			//System.out.println("DEBUG"+"\n\n\n\n");
-			map.put("Title",t2);
-			map.put("ID", t1);
-			map.put("Description",t3);
-			map.put("Detail_link","http://omim.org/entry/"+t1);
-			
-			entries.add(map);
-			
-			
-			
-		}
-		//System.out.println("DEBUG"+"Num_results:"+totalresults);
-		//System.out.println("DEBUG"+"Search_link:http://www.ncbi.nlm.nih.gov/omim/?term="+query);
-		
-		Search_link="http://www.ncbi.nlm.nih.gov/omim/?term="+query;
-		Num_results=String.valueOf(totalresults);
-		
-		
-		
-		return createStandardJson(Search_link, Num_results, entries);
-	}
-	
-	
-	
-	@GET
-	  @Path("/status")
-	  @Produces("text/html")
-	  public Response status() {
-	    return Response
-	        .ok("<h1>This is omim json service running correctly</h1>").build();
-	  }
-	
+      json += "},";
+    }
+    // remove the last comma
+    if (String.valueOf(json.charAt(json.length() - 1)).equals(","))
+      json = json.substring(0, json.length() - 1);
+
+    json += "]";
+
+    json += "}";
+
+    return json.replace("\n", " ");
+  }
+
+  public static Document readXMLFromUrl(String url) throws IOException,
+      JSONException, SAXException, ParserConfigurationException {
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    factory.setNamespaceAware(true);
+    return factory.newDocumentBuilder().parse(new URL(url).openStream());
+  }
+
+  public static JSONArray readJsonArrayFromUrl(String url) throws IOException,
+      JSONException {
+    InputStream is = new URL(url).openStream();
+    try {
+      BufferedReader rd = new BufferedReader(new InputStreamReader(is,
+          Charset.forName("UTF-8")));
+      String jsonText = readAll(rd);
+      JSONArray json = new JSONArray(jsonText);
+      return json;
+    } finally {
+      is.close();
+    }
+  }
+
+  public static String omim(String query) throws JSONException, IOException {
+
+    String Search_link = "";
+    String Num_results = "";
+    ArrayList<HashMap<String, String>> entries = new ArrayList<HashMap<String, String>>();
+
+    JSONObject json = readJsonFromUrl("http://api.omim.org/api/entry/search?search="
+        + query + "&start=0&limit=5&apiKey=" + omimkey + "&format=json");
+
+    JSONObject temp1 = (JSONObject) json.get("omim");
+    JSONObject temp2 = (JSONObject) temp1.get("searchResponse");
+    Integer totalresults = (Integer) temp2.get("totalResults");
+    JSONArray ar1 = (JSONArray) temp2.get("entryList");
+
+    for (int i = 0; i < ar1.length(); i++) {
+
+      HashMap<String, String> map = new HashMap<String, String>();
+      JSONObject temp4 = (JSONObject) ar1.get(i);
+      JSONObject temp5 = (JSONObject) temp4.get("entry");
+      JSONObject temp6 = (JSONObject) temp5.get("titles");
+      String t2 = (String) temp6.get("preferredTitle");
+      String t1 = (String) temp5.get("mimNumber");
+      String t3 = "";
+
+      JSONObject jsontemp = readJsonFromUrl("http://api.omim.org/api/entry?mimNumber="
+          + t1 + "&include=text:description&apiKey=" + omimkey + "&format=json");
+
+      JSONObject temp7 = (JSONObject) jsontemp.get("omim");
+      JSONArray ar2 = (JSONArray) temp7.get("entryList");
+      JSONObject temp9 = (JSONObject) ar2.get(0);
+      JSONObject temp11 = (JSONObject) temp9.get("entry");
+      try {
+        JSONArray ar3 = (JSONArray) temp11.get("textSectionList");
+        JSONObject temp8 = (JSONObject) ar3.get(0);
+        JSONObject temp10 = (JSONObject) temp8.get("textSection");
+        t3 = (String) temp10.get("textSectionContent");
+
+      } catch (Exception e) {
+
+      }
+
+      // System.out.println("DEBUG"+"Title:"+t2);
+      // System.out.println("DEBUG"+"ID:"+t1);
+      // System.out.println("DEBUG"+"Description:"+t3);
+      // System.out.println("DEBUG"+"Detail_link:http://omim.org/entry/"+t1);
+      // System.out.println("DEBUG"+"\n\n\n\n");
+      map.put("Title", t2);
+      map.put("ID", t1);
+      map.put("Description", t3);
+      map.put("Detail_link", "http://omim.org/entry/" + t1);
+
+      entries.add(map);
+
+    }
+    // System.out.println("DEBUG"+"Num_results:"+totalresults);
+    // System.out.println("DEBUG"+"Search_link:http://www.ncbi.nlm.nih.gov/omim/?term="+query);
+
+    Search_link = "http://www.ncbi.nlm.nih.gov/omim/?term=" + query;
+    Num_results = String.valueOf(totalresults);
+
+    return createStandardJson(Search_link, Num_results, entries);
+  }
+
+  @GET
+  @Path("/status")
+  @Produces("text/html")
+  public Response status() {
+    return Response.ok("<h1>This is omim json service running correctly</h1>")
+        .build();
+  }
 
 }
--- shangridocs-services/src/main/java/org/shangridocs/services/uniprot/UniprotResource.java
+++ shangridocs-services/src/main/java/org/shangridocs/services/uniprot/UniprotResource.java
@@ -46,206 +46,191 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-
-
-
 @Path("/uniprot")
 public class UniprotResource {
 
-	
+  @GET
+  @Path("/search")
+  @Produces("application/json")
+  public Response createBody(@QueryParam("query") String query)
+      throws JSONException, IOException, SAXException,
+      ParserConfigurationException {
 
-	
+    return Response.ok(uniprot(query), MediaType.APPLICATION_JSON).build();
 
-	@GET
-	@Path("/search")
-	@Produces("application/json")
-	public Response createBody(@QueryParam("query") String query) throws JSONException, IOException, SAXException, ParserConfigurationException  {
+  }
 
+  private static String readAll(Reader rd) throws IOException {
+    StringBuilder sb = new StringBuilder();
+    int cp;
+    while ((cp = rd.read()) != -1) {
+      sb.append((char) cp);
+    }
+    return sb.toString();
+  }
 
+  public static JSONObject readJsonFromUrl(String url) throws IOException,
+      JSONException {
+    InputStream is = new URL(url).openStream();
+    try {
+      BufferedReader rd = new BufferedReader(new InputStreamReader(is,
+          Charset.forName("UTF-8")));
+      String jsonText = readAll(rd);
+      JSONObject json = new JSONObject(jsonText);
+      return json;
+    } finally {
+      is.close();
+    }
+  }
 
+  public static String createStandardJson(String Search_link,
+      String Num_results, ArrayList<HashMap<String, String>> entries) {
 
-		return Response.ok(uniprot(query), MediaType.APPLICATION_JSON).build();
+    String json = "";
+    json += "{";
 
-	}
+    json += "\"Num_results\":\"" + Num_results + "\",";
+    json += "\"Search_link\":\"" + Search_link + "\",";
+    json += "\"Response\":[";
+    for (HashMap<String, String> map : entries) {
+      json += "{";
+      json += "\"Title\":\"" + map.get("Title") + "\",";
+      map.remove("Title");
+      json += "\"ID\":\"" + map.get("ID") + "\",";
+      map.remove("ID");
+      json += "\"Description\":\"" + map.get("Description") + "\",";
+      // System.out.println("DEBUG"+"BOOOOO "+map.get("Description"));
+      map.remove("Description");
+      json += "\"Detail_link\":\"" + map.get("Detail_link") + "\",";
+      map.remove("Detail_link");
+      json += "\"Properties\":{";
 
-	private static String readAll(Reader rd) throws IOException {
-	    StringBuilder sb = new StringBuilder();
-	    int cp;
-	    while ((cp = rd.read()) != -1) {
-	      sb.append((char) cp);
-	    }
-	    return sb.toString();
-	  }
+      for (String key : map.keySet()) {
+        json += "\"" + key + "\":\"" + map.get(key) + "\",";
+      }
+      // remove the last comma
+      if (String.valueOf(json.charAt(json.length() - 1)).equals(","))
+        json = json.substring(0, json.length() - 1);
 
-	public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-	    InputStream is = new URL(url).openStream();
-	    try {
-	      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-	      String jsonText = readAll(rd);
-	      JSONObject json = new JSONObject(jsonText);
-	      return json;
-	    } finally {
-	      is.close();
-	    }
-	  }
-	
-	
-	public static String createStandardJson( String Search_link, String Num_results,ArrayList< HashMap<String,String>> entries){
-		
-		String json="";
-		json+="{";
-		
-		json+="\"Num_results\":\""+Num_results+"\",";
-		json+="\"Search_link\":\""+Search_link+"\",";
-		json+="\"Response\":[";
-		for(HashMap<String, String> map:entries){
-			json+="{";
-			json+="\"Title\":\""+map.get("Title")+"\",";
-			map.remove("Title");
-			json+="\"ID\":\""+map.get("ID")+"\",";
-			map.remove("ID");
-			json+="\"Description\":\""+map.get("Description")+"\",";
-			//System.out.println("DEBUG"+"BOOOOO "+map.get("Description"));
-			map.remove("Description");
-			json+="\"Detail_link\":\""+map.get("Detail_link")+"\",";
-			map.remove("Detail_link");	
-			json+="\"Properties\":{";
-			
-			for(String key:map.keySet()){
-				json+="\""+key+"\":\""+map.get(key)+"\",";
-			}
-			//remove the last comma
-			if(String.valueOf(json.charAt(json.length()-1)).equals(","))
-			json = json.substring(0, json.length()-1);
-			
-			json+="}";
-			
-			json+="},";
-		}
-		//remove the last comma
-		if(String.valueOf(json.charAt(json.length()-1)).equals(","))
-		json = json.substring(0, json.length()-1);
-				
-		json+="]";
-		
-		json+="}";
-		
-		
-		
-		
-		return json.replace("\n", " ");
-	}
-	public static Document readXMLFromUrl(String url) throws IOException, JSONException, SAXException, ParserConfigurationException {
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        factory.setNamespaceAware(true);
-        return factory.newDocumentBuilder().parse(new URL(url).openStream());	   
-	  }
+      json += "}";
 
-	public static JSONArray readJsonArrayFromUrl(String url) throws IOException, JSONException {
-	    InputStream is = new URL(url).openStream();
-	    try {
-	      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-	      String jsonText = readAll(rd);
-	      JSONArray json = new JSONArray(jsonText);
-	      return json;
-	    } finally {
-	      is.close();
-	    }
-	  }
-	
-	
-	public static String uniprot(String query) throws JSONException, IOException, SAXException, ParserConfigurationException{
-		JSONArray ar1=readJsonArrayFromUrl("http://www.uniprot.org/uniprot/?query="+query+"&columns=id&start=0&limit=5&format=json");
-		
-		
+      json += "},";
+    }
+    // remove the last comma
+    if (String.valueOf(json.charAt(json.length() - 1)).equals(","))
+      json = json.substring(0, json.length() - 1);
 
-		
-		String Search_link="";
-		String Num_results="";
-		ArrayList< HashMap<String,String>> entries=new ArrayList< HashMap<String,String>>();
-		
-		
-		for(int i=0;i<ar1.length();i++){
-			HashMap<String,String> map=new HashMap<String, String>();
-			JSONObject temp1=(JSONObject)ar1.get(i);
-			String s=(String)temp1.get("id");
-			//System.out.println("DEBUG"+"ID:"+s);
-			map.put("ID", s);
-			
-			Document doc=	 readXMLFromUrl("http://www.uniprot.org/uniprot/"+s+".xml");
-			
-			//System.out.println("DEBUG"+"Detail_link:"+"http://www.uniprot.org/uniprot/"+s);
-			map.put("Detail_link","http://omim.org/entry/"+s);
-			
-			NodeList temp=doc.getElementsByTagName("organism");
-			
-			Element e=(Element)temp.item(0);
-			temp=e.getElementsByTagName("name");
-			
-			for (int indx= 0; indx < temp.getLength(); indx++) {
-			       Element eElement = (Element) temp.item(indx);
-			       
-			       if(eElement.getAttribute("type").equals("scientific")){
-			    	  
-			              System.out.println("DEBUG"+ "Organism:"+eElement.getTextContent());
-			              map.put("Organism", eElement.getTextContent());
-			       }
-			    }
-			
-		
-			
-			 temp=doc.getElementsByTagName("protein");
-			 e=(Element)temp.item(0);
-			 
-			 temp=e.getElementsByTagName("recommendedName");
-			 e=(Element)temp.item(0);
-			 
-			 temp=e.getElementsByTagName("fullName");
-			 e=(Element)temp.item(0);
-			 
-			//System.out.println("DEBUG"+"Title:"+e.getTextContent());
-			 map.put("Title",e.getTextContent());
-			
-			 temp=doc.getElementsByTagName("gene");
-			 e=(Element)temp.item(0);
-			 
-			//System.out.println("DEBUG"+"Gene:"+e.getTextContent());
-			map.put("Gene", e.getTextContent());
-			
-			NodeList nList = doc.getElementsByTagName("comment");
-		    for (int indx= 0; indx < nList.getLength(); indx++) {
-		       Element eElement = (Element) nList.item(indx);
-		       //System.out.println(""DEBUG"+eElement.getNodeName());
-		       if(eElement.getAttribute("type").equals("function")){
-		    	  // System.out.println(""DEBUG"+"hi");
-		              System.out.println("DEBUG"+"Description:"+ eElement.getTextContent());
-		              map.put("Description",eElement.getTextContent());
-		       }
-		    }
-		    
-		    
-		   
-			
-		    entries.add(map);
-			
-			
-		}
-		
-		
-		//System.out.println("DEBUG"+"Search_link:http://www.uniprot.org/uniprot/?query="+query);
-		//System.out.println("DEBUG"+"Num_results:");
-		Search_link="http://www.uniprot.org/uniprot/?query="+query;
-		return createStandardJson(Search_link, Num_results, entries);
-	}
-	
-	
-	@GET
-	  @Path("/status")
-	  @Produces("text/html")
-	  public Response status() {
-	    return Response
-	        .ok("<h1>This is uniprot json service running correctly</h1>").build();
-	  }
-	
+    json += "]";
+
+    json += "}";
+
+    return json.replace("\n", " ");
+  }
+
+  public static Document readXMLFromUrl(String url) throws IOException,
+      JSONException, SAXException, ParserConfigurationException {
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    factory.setNamespaceAware(true);
+    return factory.newDocumentBuilder().parse(new URL(url).openStream());
+  }
+
+  public static JSONArray readJsonArrayFromUrl(String url) throws IOException,
+      JSONException {
+    InputStream is = new URL(url).openStream();
+    try {
+      BufferedReader rd = new BufferedReader(new InputStreamReader(is,
+          Charset.forName("UTF-8")));
+      String jsonText = readAll(rd);
+      JSONArray json = new JSONArray(jsonText);
+      return json;
+    } finally {
+      is.close();
+    }
+  }
+
+  public static String uniprot(String query) throws JSONException, IOException,
+      SAXException, ParserConfigurationException {
+    JSONArray ar1 = readJsonArrayFromUrl("http://www.uniprot.org/uniprot/?query="
+        + query + "&columns=id&start=0&limit=5&format=json");
+
+    String Search_link = "";
+    String Num_results = "";
+    ArrayList<HashMap<String, String>> entries = new ArrayList<HashMap<String, String>>();
+
+    for (int i = 0; i < ar1.length(); i++) {
+      HashMap<String, String> map = new HashMap<String, String>();
+      JSONObject temp1 = (JSONObject) ar1.get(i);
+      String s = (String) temp1.get("id");
+      // System.out.println("DEBUG"+"ID:"+s);
+      map.put("ID", s);
+
+      Document doc = readXMLFromUrl("http://www.uniprot.org/uniprot/" + s
+          + ".xml");
+
+      // System.out.println("DEBUG"+"Detail_link:"+"http://www.uniprot.org/uniprot/"+s);
+      map.put("Detail_link", "http://omim.org/entry/" + s);
+
+      NodeList temp = doc.getElementsByTagName("organism");
+
+      Element e = (Element) temp.item(0);
+      temp = e.getElementsByTagName("name");
+
+      for (int indx = 0; indx < temp.getLength(); indx++) {
+        Element eElement = (Element) temp.item(indx);
+
+        if (eElement.getAttribute("type").equals("scientific")) {
+
+          System.out.println("DEBUG" + "Organism:" + eElement.getTextContent());
+          map.put("Organism", eElement.getTextContent());
+        }
+      }
+
+      temp = doc.getElementsByTagName("protein");
+      e = (Element) temp.item(0);
+
+      temp = e.getElementsByTagName("recommendedName");
+      e = (Element) temp.item(0);
+
+      temp = e.getElementsByTagName("fullName");
+      e = (Element) temp.item(0);
+
+      // System.out.println("DEBUG"+"Title:"+e.getTextContent());
+      map.put("Title", e.getTextContent());
+
+      temp = doc.getElementsByTagName("gene");
+      e = (Element) temp.item(0);
+
+      // System.out.println("DEBUG"+"Gene:"+e.getTextContent());
+      map.put("Gene", e.getTextContent());
+
+      NodeList nList = doc.getElementsByTagName("comment");
+      for (int indx = 0; indx < nList.getLength(); indx++) {
+        Element eElement = (Element) nList.item(indx);
+        // System.out.println(""DEBUG"+eElement.getNodeName());
+        if (eElement.getAttribute("type").equals("function")) {
+          // System.out.println(""DEBUG"+"hi");
+          System.out.println("DEBUG" + "Description:"
+              + eElement.getTextContent());
+          map.put("Description", eElement.getTextContent());
+        }
+      }
+
+      entries.add(map);
+
+    }
+
+    // System.out.println("DEBUG"+"Search_link:http://www.uniprot.org/uniprot/?query="+query);
+    // System.out.println("DEBUG"+"Num_results:");
+    Search_link = "http://www.uniprot.org/uniprot/?query=" + query;
+    return createStandardJson(Search_link, Num_results, entries);
+  }
+
+  @GET
+  @Path("/status")
+  @Produces("text/html")
+  public Response status() {
+    return Response.ok(
+        "<h1>This is uniprot json service running correctly</h1>").build();
+  }
 
 }
